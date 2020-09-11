761ba4857127a24de5000f16b7c6f6b6b4f46eeb
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-09-11 20:04:21 +0530

    board name to repo name

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ try {
     // get environment variables
     const trello_key = process.env.TRELLO_KEY
     const trello_token = process.env.TRELLO_TOKEN
+    const repo_name = github.repository
 
     // create board with repo name
-    fetch(`https://api.trello.com/1/boards/?key=${trello_key}&token=${trello_token}&name={name}`, {
+    fetch(`https://api.trello.com/1/boards/?key=${trello_key}&token=${trello_token}&name={${repo_name}}`, {
         method: 'POST'
     })
     .then(response => {
