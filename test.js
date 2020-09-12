ed2ea0424af6682f2620ef72b00457945a11edaf
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-09-12 23:10:46 +0530

    bla

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
+// This code sample uses the 'node-fetch' library:
+// https://www.npmjs.com/package/node-fetch
 const fetch = require('node-fetch');
 
-// get board id
-const getBoardId = async function (trello_username, trello_key, trello_token, repo_name) {
-    var board_id = null;
-    fetch(`https://api.trello.com/1/members/${trello_username}/boards?key=${trello_key}&token=${trello_token}`, {
+var board_id = null
+const trello_username = 'abhisheksisodiya'
+fetch(`https://api.trello.com/1/members/${trello_username}/boards?key=c2c94fd90e1f4650e3225bbb28f6ef16&token=4d66bf4b041760ae8c790dc4d935cc4b1a27b3c134fc2bbe593c25d09b966e2e`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
@@ -18,13 +19,12 @@ const getBoardId = async function (trello_username, trello_key, trello_token, re
         .then(text => {
             incomingData = JSON.parse(text)
             for (var i = 0; i < incomingData.length; i++) {
-                if (repo_name === incomingData[i].name) {
+                if ('sisodiya2421/pythonPing' === incomingData[i].name) {
                     board_id = incomingData[i].id
+                    //console.log(incomingData[i].id)
                 }
             }
+            console.log(board_id)
             return board_id;
         })
         .catch(err => console.error(err));
-}
-
-exports.getBoardId = getBoardId;
