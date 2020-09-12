76dd0b7937289ea55b5301f736c55ca88afe202a
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-09-13 00:49:47 +0530

    ......

--- components/board_id.js
+++ components/board_id.js
@@ -1,36 +1,30 @@
 const fetch = require('node-fetch');
-const core = require('@actions/core');
-const github = require('@actions/github');
 
-const trello_key = process.env.TRELLO_KEY
-const trello_token = process.env.TRELLO_TOKEN
-const trello_username = core.getInput('trello-username');
-const repo_name = core.getInput('repo-name');
-
-var board_id = null;
 // get board id
-fetch(`https://api.trello.com/1/members/${trello_username}/boards?key=${trello_key}&token=${trello_token}`, {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json'
-    }
-})
-.then(response => {
-    console.log(
-        `Response: ${response.status} ${response.statusText}`
-        );
-        return response.text();
-    })
-    .then(text => {
-        incomingData = JSON.parse(text)
-        for (var i = 0; i < incomingData.length; i++) {
-            if (repo_name === incomingData[i].name) {
-                board_id = incomingData[i].id
-            }
+const getBoardId = async function (trello_username, trello_key, trello_token, repo_name) {
+    var board_id = null;
+    return fetch(`https://api.trello.com/1/members/${trello_username}/boards?key=${trello_key}&token=${trello_token}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
         }
-        return board_id;
     })
-    .catch(err => console.error(err));
+    .then(response => {
+        console.log(
+            `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        })
+        .then(text => {
+            incomingData = JSON.parse(text)
+            for (var i = 0; i < incomingData.length; i++) {
+                if (repo_name === incomingData[i].name) {
+                    board_id = incomingData[i].id
+                }
+            }
+            return board_id;
+        })
+        .catch(err => console.error(err));
+}
 
-
-exports.fetch = fetch;
+exports.getBoardId = getBoardId;
