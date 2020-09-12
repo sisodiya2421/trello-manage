6507764f33287a11b6596ec016e3302c02ee6a7e
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-09-12 22:41:45 +0530

    test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const main = async () => {
         const trello_token = process.env.TRELLO_TOKEN
         
         // check if board is present
-        let board_id = get_board_id.getBoardId(trello_username, trello_key, trello_token, repo_name);
+        let board_id = await get_board_id.getBoardId(trello_username, trello_key, trello_token, repo_name);
         console.log(`printing ${board_id}`)
         // if (!board_id) {
         //     create_board.createBoard(trello_key, trello_token, repo_name)
